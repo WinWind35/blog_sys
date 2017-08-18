6a13263fb96280e41d0af34b168863a4ae473bb7
WinWind35 <472981667@qq.com>
2017-08-18 16:51:50 +0800

    build blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,57 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category, Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'desc', 'category',
+                    'content', 'status', 'owner', 'created_time']
+    list_filter = ('category', 'tags', 'status', 'created_time')
+    search_fields = ('title', 'desc', 'owner')
+    fieldsets = (
+        (None,{
+           'fields':(
+               'title',
+               'desc',
+               ('category', 'tags'),
+               'content',
+               ('status', 'owner'),
+           )
+        }),
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'status', 'is_nav', 'owner', 'created_time')
+    list_filter = ('status', 'is_nav', 'created_time')
+    search_fields = ('name', 'owner')
+    fieldsets = (
+        (None,{
+           'fields':(
+               'name',
+               ('status', 'is_nav'),
+               'owner',
+           )
+        }),
+    )
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'status', 'owner', 'created_time')
+    list_filter = ('status', 'created_time')
+    search_fields = ('name', 'owner')
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'status',
+                'owner',
+            )
+        }),
+    )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
